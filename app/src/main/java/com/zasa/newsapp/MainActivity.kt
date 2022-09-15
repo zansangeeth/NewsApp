674d9ee76fae0591b2fd9f2544ace14882a9daf8
zansangeeth <zansangeeth17@gmail.com>
2022-09-15 14:07:44 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/newsapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.zasa.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.zasa.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        setupWithNavController(binding.bottomNavigationView, navController)
+
+
+    }
+}
