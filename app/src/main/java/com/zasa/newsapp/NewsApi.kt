960993316337e71649496a6dcc1c006aad2730ba
zansangeeth <zansangeeth17@gmail.com>
2022-09-15 14:44:12 +0530

    implemented data classes

---
+++ app/src/main/java/com/zasa/newsapp/NewsApi.kt
@@ -0,0 +1,19 @@
+package com.zasa.newsapp
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.security.Key
+
+/**
+ **@Project -> NewsApp
+ **@Author -> Sangeeth on 9/15/2022
+ */
+interface NewsApi {
+
+    @GET("/top-headlines")
+    fun getNews(
+        @Query("apiKey") apiKey : String,
+        @Query("country") country : String
+    )
+}
---
+++ app/src/main/java/com/zasa/newsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.zasa.newsapp
+
+data class Source(
+    val id: Any,
+    val name: String
+)
